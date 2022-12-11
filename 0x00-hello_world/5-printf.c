18d0ead60b85c2d18521baf32868099e20052603
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-11 03:59:31 -0800

    a script to print outputs using printf function.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-/** Main- The Entry Point
- *
- * Return 0 (sucess)
- */
         printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
-
+        return (0);
 }
 
