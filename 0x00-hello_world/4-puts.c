e5861af9f05bfa090728d69db7e6b39c16a7a882
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-11 04:06:33 -0800

    a script to print outputs using puts function.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -5,7 +6,7 @@
  */
 int main(void)
 { 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
      	return (0);
 }
 
