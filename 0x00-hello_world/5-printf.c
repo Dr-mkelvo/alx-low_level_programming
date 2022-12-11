1c474dd86df273d1ea181305130209940fc5bd63
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-11 03:43:54 -0800

    a script to print outputs using printf function.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@ int main(void)
  *
  * Return 0 (sucess)
  */
-        printf("with proper grammar, but the outcome is a piece of art,");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 
 }
