1a36f63d8175f0136c69047f24a6df3f218d8f2e
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-26 07:12:29 -0800

    using putchar funtion to print out a-z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,11 @@
  */
 int main(void)
 {
-char ch;
+char c;
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
+for (c = 'a' ; c <= 'z' ; c++)
 {
-putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-putchar(ch);
+putchar(c);
 }
 putchar('\n');
 return (0);
