1eb3717002cd59101de51b9b31bf0dbc66c39da2
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-26 07:26:31 -0800

    Using putchar to printout a-z then A-Z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {
-		char a;
-		for (a='a';a<='z';a++)
-			putchar(a);
-		for (a ='A';a<='Z';a++)
-			putchar(a);
+	char a;
+	for (a='a';a<='z';a++)
+		putchar(a);
+	for (a ='A';a<='Z';a++)
+		putchar(a);
 		putchar('\n');
-		return (0);
+	return (0);
 }
