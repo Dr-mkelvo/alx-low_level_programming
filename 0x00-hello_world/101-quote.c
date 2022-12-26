cfee914b86f7271b991eef51927886b03281aae9
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-26 00:37:05 -0800

    Printinga stringwq without use of printf or puts function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return : 0 success
+ */
+int main ()
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
