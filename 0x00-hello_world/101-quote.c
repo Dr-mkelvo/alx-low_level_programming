c1909bc8026ce852ff71feeca33a5485868248fe
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-26 00:42:08 -0800

    Printinga string without use of printf or puts function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main ()
 {
-	ltrace("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
