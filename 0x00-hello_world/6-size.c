a65948b5599ca7a3a473daa7e98fbf2b596a7ddc
Dr-mkelvo <kelvincent002@gmail.com>
2022-12-26 00:12:43 -0800

    cheaking size of various variable types using sizeof() operator.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	return 0;
+	return (0);
 }
