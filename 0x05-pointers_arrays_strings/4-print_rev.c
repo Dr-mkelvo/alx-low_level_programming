1c68e2b356fa48b35769ac82519c3e6dd8891680
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-02 07:22:13 -0800

    a function that prints a string of unknown length in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+	while (s[length] != '\0')
+		n++;
+	for (i = length -1; i >= 0; i--)
+	{	
+		_putchar([i]);
+	}
+	_putchar('\0\);
+}
