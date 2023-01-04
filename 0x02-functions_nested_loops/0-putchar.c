31ce4cf378b7ff1b84e61e0af24b335e8349b457
Dr-mkelvo <kelvincent002@gmail.com>
2023-01-04 01:44:57 -0800

    a program that prints _putchar, followed by a new line -using a User defined function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@
 */
 int main(void)
 {
-	char *str = "_putchar";
-
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
