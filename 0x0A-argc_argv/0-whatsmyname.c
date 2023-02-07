cc1ecd13b705010df99b07f838321fdd05def31f
Dr-mkelvo <kelvincent002@gmail.com>
2023-02-07 03:33:32 -0800

    Writing a program followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints its name, followed by a new line.
  * @argc: argument count
- * @argv: arguments
+ * @argv: arguments 
  *
- * Return: 0
+ * Return: 0 Always success
  */
 int main(int argc, char **argv)
 {
