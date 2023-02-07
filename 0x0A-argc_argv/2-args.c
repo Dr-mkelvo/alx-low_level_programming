eec95858c4e92aa7c5f95c3c475f3441d453cb96
Dr-mkelvo <kelvincent002@gmail.com>
2023-02-07 03:40:54 -0800

    All arguments should be printed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - prints all arguments it receives.
  * @argc: argument count
  * @argv: arguments
  *
- * Return: 0
+ * Return: 0 Always sucess
  */
 int main(int argc, char **argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
-
+	}
 	return (0);
 }
